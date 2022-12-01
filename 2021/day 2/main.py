640c5098432ae1e09e8f12be91c474cb89231828
SturmEnte <sturmente@proton.me>
2022-12-01 19:15:08 +0100

    Finished 2022 day 2 part 1

---
+++ 2021/day 2/main.py
@@ -0,0 +1,22 @@
+h_pos = 0
+depth = 0
+
+file = open("input.txt")
+split = file.read().split("\n")
+split.pop() # Removes the last empty line
+
+# Part 1
+for input in split:
+    input = input.split(" ")
+    
+    if input[0] == "forward":
+        h_pos += int(input[1])
+    elif input[0] == "up":
+        depth -= int(input[1])
+    elif input[0] == "down":
+        depth += int(input[1])
+    else:
+        print("Unknown input:", input[0], input[1])
+
+print("Horizontal position:", h_pos, "\nDepth:", depth)
+print("Solution:", h_pos * depth)
