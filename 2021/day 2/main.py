42dffbe0819399fc6e6f6f29dd65d6170a645f47
SturmEnte <sturmente@proton.me>
2022-12-01 20:11:52 +0100

    Finished 2021 day 2 part 2

--- 2021/day 2/main.py
+++ 2021/day 2/main.py
@@ -17,5 +17,28 @@ for input in split:
     else:
         print("Unknown input:", input[0], input[1])
 
+print("Part 1:")
+print("Horizontal position:", h_pos, "\nDepth:", depth)
+print("Solution:", h_pos * depth)
+
+# Part 2
+h_pos = 0
+depth = 0
+aim = 0
+
+for input in split:
+    input = input.split(" ")
+    
+    if input[0] == "forward":
+        h_pos += int(input[1])
+        depth += aim * int(input[1])
+    elif input[0] == "up":
+        aim -= int(input[1])
+    elif input[0] == "down":
+        aim += int(input[1])
+    else:
+        print("Unknown input:", input[0], input[1])
+
+print("Part 2:")
 print("Horizontal position:", h_pos, "\nDepth:", depth)
 print("Solution:", h_pos * depth)
