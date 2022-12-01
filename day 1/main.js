a8cd1c73c768bfcda22ca6474aff17f7a0613879
SturmEnte <sturmente@proton.me>
2022-12-01 15:36:46 +0100

    Finished day 1 part 1

--- day 1/main.js
+++ day 1/main.js
@@ -1,1 +1,38 @@
 const fs = require("fs");
+const { join } = require("path");
+
+let elfs = [0];
+
+fs.readFile(join(__dirname, "input.txt"), "utf8", (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	// Add the amount of calories for each elv togethre
+	let i = 0;
+	let splitData = data.split("\n");
+	for (let j = 0; j < splitData.length; j++) {
+		let data = splitData[j];
+
+		if (data == "\r") {
+			i++;
+			elfs.push(0);
+			continue;
+		}
+
+		elfs[i] += Number(data);
+	}
+
+	// Printing the elf with the most calories
+	let most = 0;
+	elfs.forEach((elf) => {
+		if (elf > most) {
+			most = elf;
+		}
+	});
+
+	console.log(
+		"The elf carrying the most calories is carrying " + most + " calories"
+	);
+});
