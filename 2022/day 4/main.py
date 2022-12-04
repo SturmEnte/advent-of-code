2b2d44811ad2f0a4d21760328c33f54a09317383
SturmEnte <sturmente@proton.me>
2022-12-04 12:56:34 +0100

    Finished 2022 day 4 part 1

---
+++ 2022/day 4/main.py
@@ -0,0 +1,22 @@
+file = open("input.txt", "r")
+inputs = file.read().split("\n")
+
+fully_contained = 0
+
+for input in inputs:
+    start1, end1 = input.split(",")[0].split("-")
+    start2, end2 = input.split(",")[1].split("-")
+
+    start1 = int(start1)
+    start2 = int(start2)
+    end1 = int(end1)
+    end2 = int(end2)
+
+    if start1 <= start2 and end1 >= end2:
+        fully_contained += 1
+        print(start1, end1, start2, end2)
+    elif start2 <= start1 and end2 >= end1:
+        fully_contained += 1
+        print(start1, end1, start2, end2)
+
+print("Fully contained:",fully_contained)
