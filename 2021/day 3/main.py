7c434485e203d6035f0be73d11f29ff7946b2ee1
SturmEnte <sturmente@proton.me>
2022-12-04 17:05:05 +0100

    Started 2021 day 3 part 2
    I might finished it some day but at the moment it's to time comsuming

--- 2021/day 3/main.py
+++ 2021/day 3/main.py
@@ -38,3 +38,62 @@ for i in range(width):
 print("Gamma rate:", int(gamma_rate, 2))
 print("Epsilon rate:", int(epsilon_rate, 2))
 print("Submarine power comsumption:", int(gamma_rate, 2) * int(epsilon_rate, 2))
+
+# Part 2
+
+ogr = 0 # Oxyen generator rating
+co2 = 0 # CO2 scrubber rating
+
+# Turn the data into an 2 dimentional array ([x][y])
+data = []
+
+for j in range(width):
+        data.append([])
+
+for row in input:
+        for j, cell in enumerate([char for char in row]):
+                data[j].append(int(cell))
+
+input = inputs
+i = 0
+
+odata = data
+cdata = data
+
+while i < width:
+        
+        # Oxygen
+        x = 0
+
+        for cell in odata[i]:
+                x += cell
+
+        mc = x / height
+
+        if mc < 0.5:                    # More zeros than ones 
+                new = []
+                for cell in odata[i]:
+                        if cell == 0:
+                                new.append
+        elif mc > 0.5 or mc == 0.5:     # More ones than zeros or Equal amount of zeros and ones
+                continue
+
+        # CO2
+        x = 0
+
+        for cell in cdata[i]:
+                x += cell
+
+        mc = x / height
+
+        if mc < 0.5:                    # More zeros than ones or Equal amount of zeros and ones
+                continue
+        elif mc > 0.5 or mc == 0.5:     # More ones than zeros
+                continue
+
+        i += 1
+        
+
+print("Oxigen generator rating:", ogr)
+print("CO2 scrubber rating:", co2)
+print("Life support rating:", ogr * co2)
