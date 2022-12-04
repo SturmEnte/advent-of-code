7616837bb48ab776a246bf1a26a11147bd3064e5
SturmEnte <sturmente@proton.me>
2022-12-04 13:04:04 +0100

    Finished 2022 day 4 part 2

--- 2022/day 4/main.py
+++ 2022/day 4/main.py
@@ -20,3 +20,21 @@ for input in inputs:
         print(start1, end1, start2, end2)
 
 print("Fully contained:",fully_contained)
+
+overlaping = 0
+
+for input in inputs:
+    start1, end1 = input.split(",")[0].split("-")
+    start2, end2 = input.split(",")[1].split("-")
+
+    start1 = int(start1)
+    start2 = int(start2)
+    end1 = int(end1)
+    end2 = int(end2)
+
+    if end1 >= start2 and end1 <= end2:
+        overlaping += 1
+    elif end2 >= start1 and end2 <= end1:
+        overlaping += 1
+
+print("Overlaping:",overlaping)
