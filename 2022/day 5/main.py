8b6328de4bc73b726b1cd9c14ad1fe86e7884f63
SturmEnte <sturmente@proton.me>
2022-12-05 16:04:25 +0100

    Finished 2022 day 5 part 2

--- 2022/day 5/main.py
+++ 2022/day 5/main.py
@@ -1,3 +1,5 @@
+import copy
+
 def split_string(string, split_string):
     return [string[i:i+split_string] for i in range(0, len(string), split_string)]
 
@@ -37,15 +39,32 @@ for i, line in enumerate(crate_lines):
 print("Stacks:", stacks)
 print("Crates:", crates)
 
+cm9000_crates = copy.deepcopy(crates)
+
 for move in moves:
     amount = int(move.split(" ")[1])
     from_stack = int(move.split(" ")[3]) - 1
     to_stack = int(move.split(" ")[5]) - 1
     for i in range(amount):
-        crates[to_stack].insert(0, crates[from_stack].pop(0))
+        cm9000_crates[to_stack].insert(0, cm9000_crates[from_stack].pop(0))
 
 solution = ""
-for crate in crates:
+for crate in cm9000_crates:
     solution += crate[0]
 
-print("Solution for part 1:", solution)
+print("Solution for the CrateMover 9000:", solution)
+
+cm9001_crates = copy.deepcopy(crates)
+
+for move in moves:
+    amount = int(move.split(" ")[1])
+    from_stack = int(move.split(" ")[3]) - 1
+    to_stack = int(move.split(" ")[5]) - 1
+    for i in range(amount):
+        cm9001_crates[to_stack].insert(i, cm9001_crates[from_stack].pop(0))
+
+solution = ""
+for crate in cm9001_crates:
+    solution += crate[0]
+
+print("Solution for the CrateMover 9001:", solution)
