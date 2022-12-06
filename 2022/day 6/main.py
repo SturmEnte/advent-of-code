8c22f862e6e2c8d05922c92b3b54446654b74a2a
SturmEnte <sturmente@proton.me>
2022-12-06 15:39:15 +0100

    Finished 2022 day 6 part 2

--- 2022/day 6/main.py
+++ 2022/day 6/main.py
@@ -6,25 +6,49 @@ last_4_chars = []
 for i, char in enumerate(data):
     last_4_chars.append(char)
 
-    if len(last_4_chars) > 4:
+    while len(last_4_chars) > 4:
         last_4_chars.pop(0)
 
-    x = False
+    received = False
     if len(last_4_chars) == 4:
-        x = True
-        # Compare oldest char with other chars
+        received = True
         for j in range(3):
             if last_4_chars[0] == last_4_chars[j + 1]:
-                x = False
+                received = False
         
         for j in range(2):
             if last_4_chars[1] == last_4_chars[j + 2]:
-                x = False
+                received = False
 
         for j in range(1):
             if last_4_chars[2] == last_4_chars[j + 3]:
-                x = False
+                received = False
 
     if x == True:
         print("The first start-of-packet marker is received after", i + 1, "bytes")
         break
+else:
+    print("Never received an start-of-packet marker :(")
+
+# Part 2
+last_14_chars = []
+for b, char in enumerate(data):
+    last_14_chars.append(char)
+
+    while len(last_14_chars) > 14:
+        last_14_chars.pop(0)
+
+    received = False
+    if len(last_14_chars) == 14:
+        received = True
+        # This is NOT an efficient comparision technique because the bytes are compared multiple times with each other
+        for j, char in enumerate(last_14_chars):
+            for i in range(len(last_14_chars)):
+                if i != j and char == last_14_chars[i]:
+                    received = False
+
+    if received == True:
+        print("The first start-of-message marker is received after", b + 1, "bytes")
+        break
+else:
+    print("Never received an start-of-message marker :(")
