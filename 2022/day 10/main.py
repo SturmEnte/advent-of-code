806b8b3f69aef31edd44d301e6ef3af6d7adff61
SturmEnte <sturmente@proton.me>
2022-12-10 18:49:20 +0100

    Finished 2022 day 10 part 2

--- 2022/day 10/main.py
+++ 2022/day 10/main.py
@@ -1,4 +1,8 @@
 file = open("input.txt", "r")
+input = file.read()
+
+# Part 1
+print("---Part 1---")
 
 x_register = 1
 cycles = 0
@@ -6,7 +10,7 @@ signal_strengths = []
 
 checks = [20, 60, 100, 140, 180, 220]
 
-for line in file.read().split("\n"):
+for line in input.split("\n"):
     cmd = line.split()[0]
 
     if cmd == "noop":
@@ -15,7 +19,6 @@ for line in file.read().split("\n"):
         cycles += 2
 
     if cycles >= checks[0]:
-        print(checks[0],x_register)
         signal_strengths.append((x_register) * checks[0])
         checks.pop(0)
 
@@ -29,5 +32,73 @@ signal_strength_sum = 0
 for strength in signal_strengths:
     signal_strength_sum += strength
 
-print("---Part 1---")
 print("Signal strength sum:", signal_strength_sum)
+
+# Part 2
+print("")
+print("---Part 2---")
+
+width = 40
+height = 6
+
+x = 0
+y = 0
+
+x_register = 1
+
+screen = []
+
+for _ in range(height):
+    screen.append([])
+
+def cycle(n):
+    global x, y, screen, x_register
+
+    # Start of cycle
+
+
+    # During cycle
+    if x == x_register - 1 or x == x_register or x == x_register + 1:
+        screen[y].append("#")
+    else:
+        screen[y].append(".")
+
+    print(x_register)
+
+    # End of cycle
+    x_register += n
+
+    x += 1
+    if x == width:
+        y += 1
+        x = 0
+        if y == height:
+            return True
+    return False
+
+c = 1
+
+for line in input.split("\n"):
+    cmd = line.split()[0]
+
+    if cmd == "noop":
+        if cycle(0):
+            break
+        c += 1
+        
+    else:
+        if cycle(0):
+            break
+        c += 1
+        if cycle(int(line.split()[1])):
+            break
+        c += 1
+
+print(x, y, c)
+
+print("---Image---")
+for row in screen:
+    s = ""
+    for pixel in row:
+        s += pixel
+    print(s)
