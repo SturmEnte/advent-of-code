2e4e0f74dbdf6f07b07ec0f7993f195009e5ac5f
Jonas <sturmente@proton.me>
2023-12-04 19:51:43 +0100

    Started 2023/day 3/part 1

--- 2023/day 3/main.py
+++ 2023/day 3/main.py
@@ -7,18 +7,64 @@ result = 0
 
 list = []
 
-for line in input:
+for i, line in enumerate(input):
     
+    if i == 0 or line == "":
+        newLine = []
+        for _ in range(0, len(line)):
+            newLine.append(".")
+        list.append(newLine)
+
     if line == "":
         break
 
-    newLine = []
+    newLine = ["."]
     
     for symbol in line:
         newLine.append(symbol)
 
+    newLine.append(".")
+
     list.append(newLine)
 
-print(list)
+for y, ye in enumerate(list):
+    for x, xe in enumerate(ye):
+        
+        if not xe.isdigit() or ye[x-1].isdigit():
+            continue
+        
+        number_length = 0
+        number_str = ""
 
-print(f"Result:{result}")
+        for char in ye[x:]:
+            if char.isdigit():
+                number_length += 1
+                number_str += char
+                continue
+            break
+
+        number = int(number_str)
+
+        print(f"Number: {number} | Length: {number_length}")
+
+        add = False
+
+        if x-1 >= 0 and not ye[x - 1].isdigit() and ye[x - 1] != ".":
+            add = True
+        elif x+1 <= len(ye) and not ye[x + number_length].isdigit() and ye[x + number_length] != ".":
+            add = True
+
+        for entry in list[y - 1][x-1:x+number_length]:
+            if not entry.isdigit() and entry != ".":
+                add = True
+                break
+        
+        for entry in list[y + 1][x-1:x+number_length]:
+            if not entry.isdigit() and entry != ".":
+                add = True
+                break
+
+        result += number
+            
+
+print(f"Result: {result}")
