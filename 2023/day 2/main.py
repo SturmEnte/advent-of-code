615fe814f5adaf4cba8a830505aa1aad6e3cbacb
Jonas <sturmente@proton.me>
2023-12-04 11:01:08 +0100

    Finished 2023/day 2/part 2

--- 2023/day 2/main.py
+++ 2023/day 2/main.py
@@ -49,4 +49,52 @@ print(f"Result: {result}")
 print("---Part 2---")
 result = 0
 
+for line in inputs:
+
+    if line == "":
+        break
+
+    id = int(line.split(":")[0].split(" ")[1])
+
+    possible = True
+
+    lred = 0
+    lgreen = 0
+    lblue = 0
+
+    for grab in line.split(":")[1].split(";"):
+        
+        red = 0
+        green = 0
+        blue = 0
+
+        for cubes in grab.split(","):
+            split = cubes[1:].split(" ")
+
+            if split[1] == "red":
+                red += int(split[0])
+            elif split[1] == "green":
+                green += int(split[0])
+            elif split[1] == "blue":
+                blue += int(split[0])
+            else:
+                print(Back.YELLOW + f"Error: {split[0]}" + Style.RESET_ALL)
+
+        if red > lred:
+            lred = red
+
+        if green > lgreen:
+            lgreen = green
+
+        if blue > lblue:
+            lblue = blue  
+
+    result += lred * lgreen * lblue
+
+    # if possible:
+    #     result += id
+    #     print(Back.GREEN + f"{id}: r: {red} g: {green} b: {blue}"+ Style.RESET_ALL)
+    # else:
+    #     print(Back.RED + f"{id}: r: {red} g: {green} b: {blue}"+ Style.RESET_ALL)
+
 print(f"Result: {result}")
