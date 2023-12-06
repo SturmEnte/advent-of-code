7455fa7cffb3ab2847929266491605316d08234c
Jonas <sturmente@proton.me>
2023-12-06 20:21:27 +0100

    2023/day 5 - Added input and basic file layout

---
+++ 2023/day 5/main.py
@@ -0,0 +1,14 @@
+file = open("input.txt", "r")
+input = file.read().split("\n")
+
+# Part 1
+print("---Part 1---")
+result = 0
+
+print(f"Result: {result}")
+
+# Part 2
+print("---Part 2---")
+result = 0
+
+print(f"Result: {result}")
