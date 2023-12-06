4f0ff73f4575be772cac39235b42ab03d02e0802
Jonas <sturmente@proton.me>
2023-12-06 21:31:35 +0100

    Finished 2023/day 5/part 1 with example input
    My solution is to inefficient for the real input data. I have to find a more efficient solution.

--- 2023/day 5/main.py
+++ 2023/day 5/main.py
@@ -1,9 +1,58 @@
+from colorama import Back, Style
+
 file = open("example-input.txt", "r")
 input = file.read().split("\n")
 
 # Part 1
 print("---Part 1---")
 result = 0
+
+ids = []
+new_ids = []
+
+processing = False
+
+for i, line in enumerate(input):
+
+    if line == "":
+        processing = False
+        for id in new_ids:
+            ids.append(id)
+        
+        new_ids = []
+        
+        print(ids)
+        continue
+    
+    if i == 0:
+        for seed in line.split(":")[1].split(" "):
+            if seed == "":
+                continue
+            ids.append(int(seed))
+        print(f"{Back.BLUE}seeds{Style.RESET_ALL}")
+        continue
+
+    if processing == False and line.find("map:") >= 0:
+        processing = True
+        print(f"{Back.BLUE}{line.split(" ")[0]}{Style.RESET_ALL}")
+        continue
+
+    destination_start, source_start, length = line.split(" ")
+
+    destination_start = int(destination_start)
+    source_start = int(source_start)
+    length = int(length)
+
+    for i in range(0, length):
+        destination = destination_start + i
+        source = source_start + i
+
+        for id in ids:
+            if id == source:
+                new_ids.append(destination)
+                ids.remove(id)
+
+result = min(ids)
 
 print(f"Result: {result}")
 
