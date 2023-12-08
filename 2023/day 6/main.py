cd85ee8c2b936f5075708d7aadcd195088f7b6b6
Jonas <sturmente@proton.me>
2023-12-08 10:53:50 +0100

    Finished 2023/day 6/part 2

--- 2023/day 6/main.py
+++ 2023/day 6/main.py
@@ -3,36 +3,45 @@ from colorama import Back, Style
 file = open("input.txt", "r")
 input = file.read().split("\n")
 
-def parse():
-    raw_times = input[0].split(":")[1].split(" ")
-    raw_distances = input[1].split(":")[1].split(" ")
-
-    times = []
-    distances = []
-
-    for time in raw_times:
-        if time != "":
-            times.append(int(time))
-
-    for distance in raw_distances:
-        if distance != "":
-            distances.append(int(distance))
-
-    if len(times) != len(distances):
-        print(f"{Back.RED}There is a not equal amount of times and distances{Style.RESET_ALL}")
-
-    races = []
-
-    for i in range(len(times)):
-        races.append((times[i], distances[i]))
-
-    return races
-
 # Part 1
 print("---Part 1---")
+def parse(part):
+    if part == 1:
+        raw_times = input[0].split(":")[1].split(" ")
+        raw_distances = input[1].split(":")[1].split(" ")
+
+        times = []
+        distances = []
+
+        for time in raw_times:
+            if time != "":
+                times.append(int(time))
+
+        for distance in raw_distances:
+            if distance != "":
+                distances.append(int(distance))
+
+        if len(times) != len(distances):
+            print(f"{Back.RED}There is a not equal amount of times and distances{Style.RESET_ALL}")
+
+        races = []
+
+        for i in range(len(times)):
+            races.append((times[i], distances[i]))
+
+        return races
+    elif part == 2:
+        raw_time = input[0].split(":")[1]
+        raw_distance = input[1].split(":")[1]
+
+        time = int(raw_time.replace(" ", ""))
+        distance = int(raw_distance.replace(" ", ""))
+
+        return (time, distance)
+
 result = 1
 
-races = parse()
+races = parse(1)
 
 for race in races:
     time = race[0]
@@ -48,5 +57,27 @@ for race in races:
 
     print(possibilities)
     result *= len(possibilities)
+
+print(f"Result: {result}")
+
+# Part 2
+print("---Part 2---")
+result = 1
+
+race = parse(2)
+
+time = race[0]
+record = race[1]
+
+possibilities = []
+
+for speedup_time in range(0, time):
+    travel_time = time - speedup_time
+    distance = speedup_time * travel_time
+    if distance > record:
+        possibilities.append(speedup_time)
+
+# print(possibilities)
+result *= len(possibilities)
 
 print(f"Result: {result}")
