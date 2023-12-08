30cd35f3d3249dbe6a21e397d9e23f888e687e23
Jonas <sturmente@proton.me>
2023-12-08 11:46:39 +0100

    Updated base-file

--- 2023/day 7/main.py
+++ 2023/day 7/main.py
@@ -5,15 +5,17 @@ input = file.read().split("\n")
 
 # Part 1
 print(f"{Back.LIGHTWHITE_EX}---Part 1---{Style.RESET_ALL}")
-
 result = 0
+
+
 
 print(f"Result: {result}")
 
 # Part 2
 print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
-
 result = 0
+
+
 
 print(f"Result: {result}")
 
