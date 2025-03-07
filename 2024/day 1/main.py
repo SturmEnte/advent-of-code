d30b18d1d3ee53a7613cb7281e479252294801b8
Jonas <jonas@sturmente.de>
2025-03-07 21:48:08 +0100

    2024 Day 1 Part 1

---
+++ 2024/day 1/main.py
@@ -0,0 +1,36 @@
+from colorama import Back, Style
+
+file = open("input.txt", "r")
+input = file.read().split("\n")
+
+# Part 1
+print(f"{Back.LIGHTWHITE_EX}---Part 1---{Style.RESET_ALL}")
+result = 0
+
+left = []
+right = []
+
+for line in input:
+    left.append(line.split("   ")[0])
+    right.append(line.split("   ")[1])
+
+left.sort()
+right.sort()
+
+for i in range(len(left)):
+    if left[i] > right[i]:
+        result += int(left[i]) - int(right[i])
+    else:
+        result += int(right[i]) - int(left[i])
+
+print(f"Result: {result}")
+
+# Part 2
+print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
+result = 0
+
+
+
+print(f"Result: {result}")
+
+file.close()
