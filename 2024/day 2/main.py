e7280aa376926234f8c3c263447aebd75b25ab11
Jonas <jonas@sturmente.de>
2025-03-07 23:23:49 +0100

    started 2024 day 2 part 2

--- 2024/day 2/main.py
+++ 2024/day 2/main.py
@@ -1,6 +1,6 @@
 from colorama import Back, Style
 
-file = open("input.txt", "r")
+file = open("example.txt", "r")
 input = file.read().split("\n")
 
 # Part 1
@@ -54,6 +54,67 @@ print(f"Result: {result}")
 # Part 2
 print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
 result = 0
+
+def check_safety(levels, already_checked):
+    safe = True
+
+    decreasing = False
+
+    if levels[0] > levels[1]:
+        decreasing = True
+
+    problem = None
+
+    for i in range(len(levels) - 1):
+        # Check for decreasing difference safety
+        if levels[i] > levels[i + 1] and levels[i] - levels[i + 1] > 3:
+            safe = False
+            problem = i
+            break
+
+        # Check for increasing difference safety
+        if levels[i] < levels[i + 1] and levels[i + 1] - levels[i] > 3:
+            safe = False
+            problem = i
+            break
+
+        # Check if only decreasing or increasing
+        if levels[i] > levels[i + 1] and not decreasing:
+            safe = False
+            problem = i
+            break
+
+        if levels[i] < levels[i + 1] and decreasing:
+            safe = False  
+            problem = i  
+            break
+
+        if levels[i] == levels[i + 1]:
+            safe = False
+            problem = i
+            break
+
+    if not safe and not already_checked:
+        print(f"Problem at {problem} with {levels[problem]}")
+        levels.pop(problem)
+        safe = check_safety(levels, True)
+
+    return safe
+
+j = 0
+
+for report in input:
+    j += 1
+    print(f"Checking {j}")
+    levels = report.split(" ")
+
+    # Convert to integers
+    for i in range(len(levels)):
+        levels[i] = int(levels[i])
+
+    if check_safety(levels, False):
+        result += 1
+        print(f"{report} is safe")
 
 print(f"Result: {result}")
 
