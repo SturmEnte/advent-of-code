f1232ab9e7fe349f8e6c512147e6251ee9c1b00d
SturmEnte <sturmente@proton.me>
2025-03-09 13:43:32 +0100

    2015 day 1 part 1

---
+++ 2015/day 1/main.py
@@ -0,0 +1,29 @@
+from colorama import Back, Style
+
+file = open("input.txt", "r")
+input = file.read().split("\n")
+
+# Part 1
+print(f"{Back.LIGHTWHITE_EX}---Part 1---{Style.RESET_ALL}")
+result = 0
+
+for char in list(input[0]):
+    print(char)
+    if char == "(":
+        result += 1
+    elif char == ")":
+        result -= 1
+    else:
+        print("Error", char)
+
+print(f"Result: {result}")
+
+# Part 2
+print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
+result = 0
+
+
+
+print(f"Result: {result}")
+
+file.close()
