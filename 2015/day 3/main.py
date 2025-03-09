fb673812d02c916452e5bc96054cf729f97b81b3
SturmEnte <sturmente@proton.me>
2025-03-09 14:15:23 +0100

    2015 day 3 part 2

--- 2015/day 3/main.py
+++ 2015/day 3/main.py
@@ -33,7 +33,43 @@ print(f"Result: {result}")
 print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
 result = 0
 
+santa_x = 0
+santa_y = 0
 
+robo_santa_x = 0
+robo_santa_y = 0
+
+houses = [(0, 0)]
+
+for turn, direction in enumerate(list(input[0])):
+    x = 0
+    y = 0
+    
+    if direction == "^":
+        y += 1
+    elif direction == "v":
+        y -= 1
+    elif direction == ">":
+        x += 1
+    elif direction == "<":
+        x -= 1
+
+    if turn % 2 == 0:
+        santa_x += x
+        santa_y += y
+
+        if (santa_x, santa_y) not in houses:
+            houses.append((santa_x, santa_y))
+    else:
+        robo_santa_x += x
+        robo_santa_y += y
+
+        if (robo_santa_x, robo_santa_y) not in houses:
+            houses.append((robo_santa_x, robo_santa_y))
+
+    
+
+result = len(houses)
 
 print(f"Result: {result}")
 
