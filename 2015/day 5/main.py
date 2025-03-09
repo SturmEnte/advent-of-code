2014466a18a9a227a6e97347503df4130f194778
SturmEnte <sturmente@proton.me>
2025-03-09 22:13:48 +0100

    2015 day 5 part 2

--- 2015/day 5/main.py
+++ 2015/day 5/main.py
@@ -38,7 +38,33 @@ print(f"Result: {result}")
 print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
 result = 0
 
+for string in input:
+    string = list(string)
+    
+    pair_twice = False
+    repeat_with_one_between = False
 
+    for i, char in enumerate(string):
+
+        if i == len(string) - 2:
+            break
+
+        if char == string[i + 2]:
+            repeat_with_one_between = True
+
+        char1 = char
+        char2 = string[i + 1]
+
+        new_string = string[i + 2:]
+
+        for j, char in enumerate(new_string):
+            if j < len(new_string) - 1 and char1 == char and char2 == new_string[j + 1]:
+                pair_twice = True
+
+    print(pair_twice, repeat_with_one_between)
+
+    if pair_twice and repeat_with_one_between:
+        result += 1
 
 print(f"Result: {result}")
 
