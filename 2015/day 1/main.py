7d74f47e19b82950c40a9faf71d280c6f4c4cd5e
SturmEnte <sturmente@proton.me>
2025-03-09 13:46:17 +0100

    2015 day 1 part 2

--- 2015/day 1/main.py
+++ 2015/day 1/main.py
@@ -8,7 +8,6 @@ print(f"{Back.LIGHTWHITE_EX}---Part 1---{Style.RESET_ALL}")
 result = 0
 
 for char in list(input[0]):
-    print(char)
     if char == "(":
         result += 1
     elif char == ")":
@@ -21,8 +20,19 @@ print(f"Result: {result}")
 # Part 2
 print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
 result = 0
+floor = 0
 
+for i, char in enumerate(list(input[0])):
+    if char == "(":
+        floor += 1
+    elif char == ")":
+        floor -= 1
+    else:
+        print("Error", char)
 
+    if floor == -1:
+        result = i + 1
+        break
 
 print(f"Result: {result}")
 
