7d44eec9f7a05768653a4326ee1fc34983622fa4
SturmEnte <sturmente@proton.me>
2025-03-13 10:07:44 +0100

    2015 day 10 part 2

--- 2015/day 10/main.py
+++ 2015/day 10/main.py
@@ -29,8 +29,6 @@ for i in range(40):
             count = 1
     
     new_number += str(count) + last_processed
-    print("New number", new_number)
-
     last_number = new_number
 
 result = len(last_number)
@@ -41,7 +39,31 @@ print(f"Result: {result}")
 print(f"{Back.LIGHTWHITE_EX}---Part 2---{Style.RESET_ALL}")
 result = 0
 
+input_number = input[0]
 
+last_number = input_number
+
+for i in range(50):
+    new_number = ""
+
+    last_number_parts = list(last_number)
+
+    count = 0
+    last_processed = last_number_parts[0]
+
+    for number in last_number_parts:
+
+        if number == last_processed:
+            count += 1
+        else:
+            new_number += str(count) + last_processed
+            last_processed = number
+            count = 1
+    
+    new_number += str(count) + last_processed
+    last_number = new_number
+
+result = len(last_number)
 
 print(f"Result: {result}")
 
