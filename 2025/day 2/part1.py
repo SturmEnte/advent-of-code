14a5160befb5e1ba60f83a5384468c183da1144b
Jonas <jonas@sturmente.de>
2025-12-02 12:39:36 +0100

    2025 day 2 part 1

---
+++ 2025/day 2/part1.py
@@ -0,0 +1,36 @@
+from colorama import Back, Style
+
+file = open("input.txt", "r")
+input = file.read().split(",")
+
+# Part 1
+print(f"{Back.LIGHTWHITE_EX}---Part 1---{Style.RESET_ALL}")
+result = 0
+
+for ids in input:
+    limits = ids.split("-")
+    
+    lower = int(limits[0])
+    upper = int(limits[1])
+
+    for i in range(lower, upper + 1):
+        i_str = str(i)
+        length = len(i_str)
+
+        # All ids not dividable by 2 must be valid
+        if length % 2 != 0:
+            print(i, "valid")
+            continue
+        
+        if i_str[:length // 2] != i_str[length // 2:]:
+            print(i, "valid")
+            continue
+        
+        print(i, "invalid")
+
+        result += i
+        
+
+print(f"Result: {Back.GREEN}{result}{Style.RESET_ALL}")
+
+file.close()
