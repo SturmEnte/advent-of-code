575a5310f0599644119071d68dc43f811f54a29f
Jonas <jonas@sturmente.de>
2025-12-02 13:11:28 +0100

    2025 day 2 part 2 comments

--- 2025/day 2/part2.py
+++ 2025/day 2/part2.py
@@ -25,26 +25,31 @@ for ids in input:
     lower = int(limits[0])
     upper = int(limits[1])
 
+    # Iterate through each ID in the current range
     for i in range(lower, upper + 1):
         i_str = str(i)
         length = len(i_str)
 
-        
+        # Get all numbers from 2 to the char length of the current ID
         slicing_options = range(2, length + 1)
+        
+        # Filter out all numbers from the array from before that are not dividable by the current ID
         new_slicing_options = []
-
         for option in slicing_options:
             if length % option == 0:
                 new_slicing_options.append(option)
 
         slicing_options = new_slicing_options
 
+        # Check for a repeating pattern
         repeating_pattern = False
 
+        # For that iterate through each number the current ID is dividable by
         for option in slicing_options:
+            # Divide the ID up in the current number checked for parts
             slices = slice_string_equal_parts(i_str, option)
-            #print(slices)
 
+            # Check if all parts of the current ID are the same as the first one
             equal_parts = True
 
             for k in range(1, option):
