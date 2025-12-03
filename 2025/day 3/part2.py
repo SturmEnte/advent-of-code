f1927efc86a1f32fb718c6ca5638b0f342440b4e
SturmEnte <jonas@sturmente.de>
2025-12-03 11:08:10 +0100

    2025 day 3 part 2 comment

--- 2025/day 3/part2.py
+++ 2025/day 3/part2.py
@@ -3,6 +3,9 @@ input = file.read().split("\n")
 
 result = 0
 
+# Choose the largest number in the number array in all numbers from the first to the nth so that the last depth numbers remain for the next cycle
+# After choosing the number and storing its index give the remaining numbers (those which come after the index of the largest)
+# Do this until depth 1 is reached. Then combine all the largest numbers after the current number and return the resulting number
 def find_largest_number_of_n(numbers: list[int], depth: int) -> int:
     
     largest_number = 0
